191abbecc3fecf2391717760f49b03c31a318ce2
hamellton <hamelltonn@gmail.com>
2023-11-02 19:02:39 +0200

    added pagination to products api

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,9 +7,28 @@ const { productNotFoundMessage } = messages.productController;
 const router = express.Router();
 const productModel = new Product();
 
+// router.get("/", (req, res) => {
+//   const products = productModel.getAllProducts();
+//   res.json({ products, status: 200 });
+// });
+
 router.get("/", (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 10;
+
+  const startIndex = (page - 1) * perPage;
+  const endIndex = page * perPage;
+
   const products = productModel.getAllProducts();
-  res.json({ products, status: 200 });
+  const paginatedProducts = products.slice(startIndex, endIndex);
+
+  res.json({
+    products: paginatedProducts,
+    page,
+    perPage,
+    totalProducts: products.length,
+    status: 200,
+  });
 });
 
 router.get("/product/:id", (req, res) => {
